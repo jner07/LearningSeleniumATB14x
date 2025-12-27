e4980908ce7735f708d254461c5370312773466b
jner07 <jayeshner0705@gmail.com>
2025-12-27 18:06:21 +0530

    TTA Bank Account creation

--- src/test/java/org/example/ex03_Selenium_XPath/Lab013_Proj5_TTA_BankAccountCreation.java
+++ src/test/java/org/example/ex03_Selenium_XPath/Lab013_Proj5_TTA_BankAccountCreation.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class Lab013_Proj5_TTA_BankAccountCreation extends CommonToAll {
 
@@ -36,6 +39,34 @@ public class Lab013_Proj5_TTA_BankAccountCreation extends CommonToAll {
 
         WebElement createButton = driver.findElement(By.xpath("//button[text()='Create Account']"));
         createButton.click();
+
+        customWait(2000);
+
+        WebElement dashboard = driver.findElement(By.xpath("//h1[text()='Dashboard']"));
+        Assert.assertEquals(dashboard.getText(), "Dashboard");
+
+        WebElement TransferFundButton = driver.findElement(By.xpath("//button[text()='Transfer Funds']"));
+        TransferFundButton.click();
+
+        customWait(1000);
+
+        WebElement ManageBenf_button = driver.findElement(By.xpath("//button[text()='Manage Beneficiaries']"));
+        ManageBenf_button.click();
+
+        customWait(2000);
+
+        List<WebElement> fullname = driver.findElements(By.xpath("//input[@type='text']"));
+       fullname.get(0).sendKeys("Yoo Yoo");
+
+        List<WebElement> BankName = driver.findElements(By.xpath("//input[@type='text']"));
+        fullname.get(1).sendKeys("HDFT bank");
+
+        List<WebElement> accountNum = driver.findElements(By.xpath("//input[@type='text']"));
+        fullname.get(2).sendKeys("360258972038");
+
+        WebElement savebenf_button = driver.findElement(By.xpath("//button[text()='Save Beneficiary']"));
+        savebenf_button.click();
+
 
 
         closeBrowser(driver);
